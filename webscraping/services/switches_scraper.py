9296554140104df6b01d3e4fdb430c077d92be0b
Phone Mon <e1121278z2u.nus.edu>
2024-06-25 21:50:53 +0800

    Changed webscraper

--- webscraping/services/switches_scraper.py
+++ webscraping/services/switches_scraper.py
@@ -3,58 +3,57 @@
 from playwright.sync_api import sync_playwright
 import json
 
-
-KEYCAP_URL = "https://keeb-finder.com/keycaps"
-LUBRICANT_URL = "https://keeb-finder.com/accessories/lubrications"
-KIT_URL = "https://keeb-finder.com/keyboards"
-SWITCH_URL = "https://keeb-finder.com/switches"
+SWITCH_LISTINGS_URL = "https://keeb-finder.com/switches"
 
 
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-    page1 = browser.new_page()
+    listing_page = browser.new_page()
+    details_page = browser.new_page()
 
-    page.goto(SWITCH_URL)
-    page_number = int(page.query_selector('body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.MuiBox-root.mui-0 > nav > ul > li:nth-child(8) > a').inner_text())
+    listing_page.goto(SWITCH_LISTINGS_URL)
+    pages = int(listing_page.query_selector('body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.MuiBox-root.mui-0 > nav > ul > li:nth-child(8) > a').inner_text()) # type: ignore
     data = []
 
-    for page_no in range(1, page_number + 1, 1):
-        page.goto(f"https://keeb-finder.com/switches?page={page_no}")
-        number_of_listings = len(page.query_selector_all("article"))
+    for page in range(1, pages + 1, 1):
+        listing_page.goto(f"https://keeb-finder.com/switches?page={page}")
+        number_of_listings = len(listing_page.query_selector_all("article"))
         for i in range(1, number_of_listings + 1, 1):
-            name = page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
-                                    div.flex.flex-wrap.items-center.justify-between.mt-1.mb-1\.5.group > a.font-h4.text-h4.text-gray-900.dark\:text-gray-100.p-1.text-left.cursor-pointer.webkit-box.overflow-hidden.no-underline.line-clamp-2').inner_text()
-            price = page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
-                                        div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > div.absolute.right-1\.5.bottom-1\.5.flex.justify-end.bg-black.bg-opacity-70.p-1\.5.px-2.rounded.text-center.z-10.text-gray-100.font-medium').inner_text()
-            
+            name = listing_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
+                                               div.flex.flex-wrap.items-center.justify-between.mt-1.mb-1\.5 > a.font-h4.text-h4.text-gray-900.dark\:text-gray-100.p-1.text-left.cursor-pointer.webkit-box.overflow-hidden.no-underline.line-clamp-2').inner_text() # type: ignore
+            price = listing_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
+                                        div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > div.absolute.right-1\.5.bottom-1\.5.flex.justify-end.bg-black.bg-opacity-70.p-1\.5.px-2.rounded.text-center.z-10.text-gray-100.font-medium').inner_text() # type: ignore
+            link = listing_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
+                                               div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > a').get_attribute('href') # type: ignore
+
             try:
-                availability = page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
-                                                    div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > div.absolute.inset-0.bg-black.bg-opacity-30.z-10.pointer-events-none > div > div').inner_text()          
+                availability = listing_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({i}) > \
+                                                    div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > div.absolute.inset-0.bg-black.bg-opacity-30.z-10.pointer-events-none > div > div').inner_text() # type: ignore      
     
             except AttributeError:
                 availability = "In stock"
             
-            link = page.get_by_role('link', name=name).first().get_attribute('href')
-            page1.goto(f"https://keeb-finder.com{link}")
+            details_page.goto(f"https://keeb-finder.com{link}")
 
-            switch_type = page1.query_selector(r'body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.flex > div').inner_text()
-            brand = page1.query_selector(r'body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.flex').inner_text()
-                   
+            try:
+                switch_type = details_page.query_selector('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > \
+                                                      div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.flex > div').inner_text() # type: ignore
+                
+            except AttributeError:
+                switch_type = "No type found."
+
             data.append({
                 "name": name,
-                "price_per_unit": price,
+                "price": price,
                 "switch_type": switch_type,
-                "availability": availability,
-                "brand": brand
+                "availability": availability
             })
-
             
 
     browser.close()
 
-    with open('app/services/webscraper/data/switches.json', 'w') as f:
+    with open('webscraping/data/switches.json', 'w') as f:
             json.dump(data, f, indent=4)
 
 
--- webscraping/services/tests.py
+++ webscraping/services/tests.py
@@ -1,17 +1,87 @@
+# Loading necessary packages
+
 from playwright.sync_api import sync_playwright
 import json
+
+SWITCH_LISTINGS_URL = "https://keeb-finder.com/switches"
+
 
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
-
-    page.goto("https://keeb-finder.com/switches/gateron-x-sw-modern-gray-linear-switches")
-
+    context = browser.new_context()
+    listings_page = context.new_page()
+    details = context.new_page()
+    listings_page.goto(SWITCH_LISTINGS_URL)
+    
     data = []
-    switch_type = page.query_selector(r'body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.flex > div').inner_text()
-    brand = page.query_selector(r'body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.flex').inner_text()
-    data.append([switch_type, brand])
-    print(data)
+    articles = len(listings_page.query_selector_all("article"))
+    pages = int(listings_page.query_selector("body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > \
+                                    div.MuiBox-root.mui-0 > nav > ul > li:nth-child(8) > a").inner_text()) # type: ignore
+    for pagination in range(1, 4, 1):
+        listings_page.goto(f"https://keeb-finder.com/switches?page={pagination}")
+        listings_page.screenshot(path="example.png", full_page=True)
+        for article in range(1, articles + 1, 1):
+            name = listings_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > \
+                                                div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > \
+                                                article:nth-child({article}) > div.flex.flex-wrap.items-center.justify-between.mt-1.mb-1\.5 > \
+                                                a.font-h4.text-h4.text-gray-900.dark\:text-gray-100.p-1.text-left.cursor-pointer.webkit-box.overflow-hidden.no-underline.line-clamp-2').inner_text() # type: ignore
+            
+            price = listings_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({article}) > \
+                                                div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > \
+                                                div.absolute.right-1\.5.bottom-1\.5.flex.justify-end.bg-black.bg-opacity-70.p-1\.5.px-2.rounded.text-center.z-10.text-gray-100.font-medium').inner_text() # type: ignore
+            
+            link = listings_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > \
+                                                div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({article}) > \
+                                                div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > a').get_attribute('href') # type: ignore
+
+            try:
+                availability = listings_page.query_selector(f'body > div.flex.flex-wrap.justify-center > div.max-w-lg.w-full.px-4.my-2.z-0 > div.grid.gap-4.grid-cols-2.xs\:grid-cols-2.sd\:grid-cols-3.lg\:grid-cols-4 > article:nth-child({article}) > \
+                                                    div.aspect-\[1\/1\].relative.w-full.overflow-hidden.rounded-lg.bg-gray-200.undefined > div.absolute.inset-0.bg-black.bg-opacity-30.z-10.pointer-events-none > div > div').inner_text() # type: ignore      
+    
+            except AttributeError:
+                availability = "In stock"
+        
+            details.goto(f"https://keeb-finder.com{link}")
+            details.screenshot(path="example.png")
+
+            switch_type = None
+            manufacturer = None
+            actuation_force = None
+            travel_distance = None
+            vendors = []
+
+
+            try:
+                switch_type = details.query_selector('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > \
+                                                      div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div.flex > div').inner_text() # type: ignore
+                manufacturer = details.query_selector('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > \
+                                                      div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div.flex > div').inner_text() # type: ignore
+                actuation_force = details.query_selector('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > \
+                                                         div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div.flex > div').inner_text() # type: ignore
+                travel_distance = details.query_selector('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.md\:col-span-5.md\:order-2.order-3 > \
+                                                         div:nth-child(1) > div:nth-child(2) > div:nth-child(6) > div.flex > div').inner_text() # type: ignore
+                vendors_data = details.query_selector_all('body > div.flex.flex-col.gap-4 > section:nth-child(1) > div > div.col-span-12.order-2.md\:order-3.mt-2 > \
+                                                     div.bg-gray-50.dark\:bg-gray-700.mt-4.rounded-lg.shadow-md.border.border-gray-200.dark\:border-gray-800 > table > tbody > tr:nth-child(4) > td.flex.items-center.p-2 > div > img')
+                for i in vendors_data:
+                    vendors.append(i.get_attribute("alt"))
+
+
+            data.append({
+                "name": name,
+                "price": price,
+                "manufacturer": manufacturer,
+                "switch_type": switch_type,
+                "actuation_force": actuation_force,
+                "travel_distance": travel_distance,
+                "vendors": vendors,
+                "availability": availability
+            })
+
+            
+
+    context.close()
     browser.close()
 
+    with open('webscraping/data/tests.json', 'w') as f:
+            json.dump(data, f, indent=4)
